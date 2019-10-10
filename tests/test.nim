a70412779f7aead6f601dd6392e375b6deb49cd4
Juan Carlos <juancarlospaco@gmail.com>
2019-10-10 19:54:14 -0300

    Fix sql generation bugs

--- tests/test.nim
+++ tests/test.nim
@@ -21,20 +21,20 @@ suite "Gatabase ORM Tests":
   doAssert db.tryExec(exampleTable), "Error creating 'exampleTable'"
 
 
-  test "INSERT INTO":
+  test "let   INSERT INTO":
     let example {.used.} = query TryExec:
       insertinto "person"
       values (42, "maximus", true, "maximus.nimmer@nim-lang.org", 5.5)
 
 
-  test "SELECT ... FROM ... WHERE":
+  test "let   SELECT ... FROM ... WHERE":
     let example2 {.used.} = query TryExec:
       select '*'
       `from`"person"
       where "id = 42"
 
 
-  test "SELECT ... (comment) ... FROM ... COMMENT":
+  test "let   SELECT ... (comment) ... FROM ... COMMENT":
     let example2 {.used.} = query TryExec:
       select '*'
       `--`"This is a comment, this will be strapped for Release builds"
@@ -42,7 +42,7 @@ suite "Gatabase ORM Tests":
       comment {"on": "TABLE", "person": "This is an SQL COMMENT on a TABLE"}
 
 
-  test "SELECT ... FROM ... LIMIT ... OFFSET":
+  test "let   SELECT ... FROM ... LIMIT ... OFFSET":
     let example2 {.used.} = query TryExec:
       select '*'
       `from`"person"
@@ -50,14 +50,14 @@ suite "Gatabase ORM Tests":
       limit 2
 
 
-  test "INSERT INTO":
-    let example3 {.used.} = query TryExec:
+  test "let   INSERT INTO":
+    let example3 {.used.} = query Func:
       insertinto "person"
       values (99, "Nikola Tesla", false, "nikola.tesla@nim-lang.org", 9.6)
 
 
-  test "UNION ALL ... ORBER BY ... IS NOT NULL":
-    let example2 {.used.} = query TryExec:
+  test "let   UNION ALL ... ORBER BY ... IS NOT NULL":
+    let example2 {.used.} = query Sql:
       select '*'
       `from`"person"
       where "id = 42"
@@ -69,15 +69,70 @@ suite "Gatabase ORM Tests":
       orderby "asc"
 
 
-  test "SELECT DISTINCT ... FROM ... WHERE":
-    let example2 {.used.} = query TryExec:
+  test "let   SELECT DISTINCT ... FROM ... WHERE":
+    let example2 {.used.} = query Prepared:
       selectdistinct "id"
       `from`"person"
       where "rank != 666.0"
 
 
-  test "DELETE FROM WHERE":
-    let example9 {.used.} = query TryExec:
+  test "const INSERT INTO":
+    const example {.used.} = query Func:
+      insertinto "person"
+      values (42, "maximus", true, "maximus.nimmer@nim-lang.org", 5.5)
+
+
+  test "const SELECT ... FROM ... WHERE":
+    const example2 {.used.} = query Sql:
+      select '*'
+      `from`"person"
+      where "id = 42"
+
+
+  test "const SELECT ... (comment) ... FROM ... COMMENT":
+    const example2 {.used.} = query Prepared:
+      select '*'
+      `--`"This is a comment, this will be strapped for Release builds"
+      `from`"person"
+      comment {"on": "TABLE", "person": "This is an SQL COMMENT on a TABLE"}
+
+
+  test "const SELECT ... FROM ... LIMIT ... OFFSET":
+    const example2 {.used.} = query Func:
+      select '*'
+      `from`"person"
+      offset 0
+      limit 2
+
+
+  test "const INSERT INTO":
+    const example3 {.used.} = query Sql:
+      insertinto "person"
+      values (99, "Nikola Tesla", false, "nikola.tesla@nim-lang.org", 9.6)
+
+
+  test "const UNION ALL ... ORBER BY ... IS NOT NULL":
+    const example2 {.used.} = query Prepared:
+      select '*'
+      `from`"person"
+      where "id = 42"
+      union true
+      select '*'
+      `from`"person"
+      where "name"
+      isnull false
+      orderby "asc"
+
+
+  test "const SELECT DISTINCT ... FROM ... WHERE":
+    const example2 {.used.} = query Sql:
+      selectdistinct "id"
+      `from`"person"
+      where "rank != 666.0"
+
+
+  test "let   DELETE FROM WHERE":
+    var example9 {.used.} = query TryExec:
       delete "person"
       where "id = 42"
 
