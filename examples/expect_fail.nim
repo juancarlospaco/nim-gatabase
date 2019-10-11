b2193949471eff23ecea1eb9b41288e5acd54607
Juan Carlos <juancarlospaco@gmail.com>
2019-10-11 17:12:55 -0300

    Add some example that fail to compile intentionally

---
+++ examples/expect_fail.nim
@@ -0,0 +1,31 @@
+## Examples here Fail intentionally to desmostrate "Smart" SQL Syntax Checkings.
+## You can try uncommenting some of them and running it, they will fail.
+import ../src/gatabase
+
+
+# discard query Sql:
+#   `from` "failure"
+
+
+# discard query Sql:
+#   where "failure"
+
+
+# discard query Sql:
+#   wherenot "failure"
+
+
+# discard query Sql:
+#   whereexists "failure"
+
+
+# discard query Sql:
+#   orderby "failure"
+
+
+# discard query Sql:
+#   like "failure"
+
+
+# discard query Sql:
+#   between "failure"
