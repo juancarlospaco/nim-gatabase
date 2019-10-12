5d74f9626ed94fa09eccacc363cb75de719ffd2a
Juan Carlos <juancarlospaco@gmail.com>
2019-10-12 14:48:37 -0300

    docs

--- src/gatabase.nim
+++ src/gatabase.nim
@@ -204,8 +204,8 @@ macro query*(output: GatabaseOutput, inner: untyped): untyped =
     of "commentontable": sqls.add comments(node[1], "TABLE")
     else: doAssert false, "Unknown syntax error on ORMs DSL: " & inner.lineInfo
   when not defined(release) or not defined(danger):
-    if unlikely(deleteUsed and not whereUsed): {.warning: "DELETE FROM without WHERE".}
-  doAssert sqls.len > 0, "Unknown error on SQL DSL, SQL Query must not be empty."
+    if unlikely(deleteUsed and not whereUsed): {.warning: "DELETE FROM without WHERE.".}
+  assert sqls.len > 0, "Unknown error on SQL DSL, SQL Query must not be empty."
   sqls.add when defined(release): ";" else: ";  /* " & inner.lineInfo & " */\n"
   when defined(dev): echo sqls
   let # This prepares the arguments from a Tuple into varargs "unpacked".
