055c6f088f7ce98fd436f0496629628a4e0443de
Juan Carlos <JuanCarlospaco@gmail.com>
2020-06-29 12:27:17 -0300

    -

--- tests/test_multigata.nim
+++ tests/test_multigata.nim
@@ -15,4 +15,4 @@ var multigata = newGatabase("localhost", "postgres", "postgres", "postgres")
 doAssert multigata is Gatabase
 doAssert len(multigata) == 100
 doAssert getAllRows(multigata, query = sql"SELECT version();", @[]) is Future[seq[Row]]
-echo multigata
+echo $multigata
