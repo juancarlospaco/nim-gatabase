fde3c2c638e1ef487d81ef19a391b87047ce598a
Juan Carlos <JuanCarlospaco@gmail.com>
2020-06-29 10:47:14 -0300

    -

--- tests/test_multigata.nim
+++ tests/test_multigata.nim
@@ -12,4 +12,4 @@ const exampleTable = sql"""
   ); """
 
 var multigata = newGatabase("localhost", "postgres", "postgres", "postgres")
-waitFor getAllRows(multigata, query = sql"SELECT version();", @[])
+doAssert waitFor getAllRows(multigata, query = sql"SELECT version();", @[]) is seq[Row]
