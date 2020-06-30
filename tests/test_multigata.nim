a9a3299431b80288d1020d64ac9d4562701d6778
Juan Carlos <JuanCarlospaco@gmail.com>
2020-06-29 22:30:31 -0300

    -

--- tests/test_multigata.nim
+++ tests/test_multigata.nim
@@ -12,20 +12,18 @@ const exampleTable = sql"""
   ); """
 
 
-# Manual
 let db = newGatabase("localhost", "postgres", "postgres", "postgres")
 doAssert db is Gatabase
 doAssert len(db) == 100
 let data = wait_for getAllRows(db, query = sql"SELECT version();", @[])
 doAssert data is seq[Row]
 doAssert len(data) > 0 and len(data[0]) > 0
-echo data[0]
+echo data[0]  # Postgres 12
 doAssert execAffectedRows(db, query = sql"SELECT version();", @[]) is Future[int64]
 doAssert exec(db, query = sql"SELECT version();", @[]) is Future[void]
 for _ in 0 .. len(db) - 1: doAssert db.getAllRows(sql"SELECT version();", @[]) is Future[seq[Row]]
 
 
-# DSL
 var args: seq[string] # Just for testing, can also be @[]
 
 let dataset0: Future[seq[Row]] = args.getAllRows:
@@ -36,7 +34,8 @@ let dataset1: Future[int64] = args.execAffectedRows:
 
 exec args:
   select "version()"
+  `--`   "You can await() them too, this is just an example."
 
 
-echo $db
+echo $db # Pool of 100 connections
 db.close()
