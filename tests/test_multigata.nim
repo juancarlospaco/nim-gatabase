3db0c01197fed2d8f5a08d4ac477b10ed91b7edf
Juan Carlos <JuanCarlospaco@gmail.com>
2020-06-29 22:03:50 -0300

    -

--- tests/test_multigata.nim
+++ tests/test_multigata.nim
@@ -34,7 +34,7 @@ let dataset0: Future[seq[Row]] = args.getAllRows:
 let dataset1: Future[int64] = args.execAffectedRows:
   select "version()"
 
-wait_for args.exec:
+wait_for exec args:
   select "version()"
 
 
